803d0abb3c36541954c4cad7298c5a52cfcf40ae
sa-kevin <kevin@s-arcana.co.jp>
2024-06-20 17:05:08 +0900

    install of breeze and creat route/controller

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\OrderController;
+use App\Http\Controllers\API\OrderItemController;
+use App\Http\Controllers\API\PaymentController;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('products', ProductController::class);
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('orders', OrderController::class);
+Route::apiResource('order-items', OrderItemController::class);
+Route::apiResource('payments', PaymentController,::class);
