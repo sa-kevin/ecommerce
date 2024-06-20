70c4bf46ade7de1ff182d1d6914ca839ed26d14f
Kevin Timsiline <kevtim@ARC042MAC.local>
2024-06-20 15:43:34 +0900

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use illuminate\Database\Eloquent\Model;
+
+class Product extends Model {
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'quantity', 'category_id'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function OrderItems(){
+        return $this->hasMany(OrderItems::class);
+    }
+}
+
+?>
