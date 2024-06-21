a3c747c57648e52f99e877e23ab90a841797e1f7
sa-kevin <kevin@s-arcana.co.jp>
2024-06-21 12:12:05 +0900

    feat: table schema for my db

--- database/migrations/2024_06_20_035256_create_orders_table.php
+++ database/migrations/2024_06_20_035256_create_orders_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('total', 8, 2);
+            $table->string('status');
             $table->timestamps();
         });
     }
--- database/migrations/2024_06_20_060011_create_payments_table.php
+++ database/migrations/2024_06_20_060011_create_payments_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 8, 2);
+            $table->string('status');
+            $table->string('payment_method');
             $table->timestamps();
         });
     }
