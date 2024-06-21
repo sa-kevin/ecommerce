c0d951706893e67f7afe2ffc15b85c3d102001e4
sa-kevin <kevin@s-arcana.co.jp>
2024-06-21 11:02:03 +0900

    class typo correction

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
 
-class OrderItemController extends Controller
+class OrderController extends Controller
 {
     /**
      * Display a listing of the resource.
