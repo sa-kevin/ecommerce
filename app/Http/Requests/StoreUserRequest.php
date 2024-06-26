f9350ab948ceb27c90b854f2a1d1c75e57e7cda6
sa-kevin <kevin@s-arcana.co.jp>
2024-06-26 13:54:15 +0900

    feat: add password to user

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -25,7 +25,8 @@ class StoreUserRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users'
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
         ];
     }
 
