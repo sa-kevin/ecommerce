40da756717bcdc78eda73eb9572dab5139d1cfc5
sa-kevin <kevin@s-arcana.co.jp>
2024-06-26 13:54:50 +0900

    upgrade controller to repo partern

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,61 +2,76 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Classes\ApiResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Interfaces\UserRepositoryInterface;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    private UserRepositoryInterface $userRepositoryInterface;
+
+    // dependency injection
+    public function __construct(UserRepositoryInterface $userRepositoryInterface)
+    {
+        $this->userRepositoryInterface = $userRepositoryInterface;
+    }
     public function index()
     {
-        return User::all();
+        $data = $this->userRepositoryInterface->getAll();
+        return ApiResponseHelper::sendResponse(UserResource::collection($data), '', 200);
     }
-
-    public function store(Request $request)
+    public function show(string $id)
     {
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
-        ]);
-
-        $user = User::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'password' => Hash::make($validated['password']),
-        ]);
-
-        return response()->json($user, 201);
+        $user = $this->userRepositoryInterface->getById($id);
+        return ApiResponseHelper::sendResponse(UserResource::collection($user),'', 200);
     }
-
-    public function show($id)
+    public function store(StoreUserRequest $request)
     {
-        return User::find($id);
-    }
-
-    public function update(Request $request, $id)
-    {
-        $validated = $request->validate([
-            'name' => 'sometimes|required|string|max:255',
-            'email' => 'sometimes|required|string|email|max:255|unique:users,email,' . $id,
-            'password' => 'sometimes|required|string|min:8|confirmed',
-        ]);
-
-        $user = User::findOrFail($id);
-        $user->update($validated);
-        if ($request->has('password')) {
-            $user->password = Hash::make($validated['password']);
+        $data = [
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password'=> Hash::make($request->password),
+        ];
+        DB::beginTransaction();
+        try {
+            $user = $this->userRepositoryInterface->store($data);
+            DB::commit();
+            return ApiResponseHelper::sendResponse(new UserResource($user),'Record create succesfully',200);
+        } catch (\Exception $ex) {
+            DB::rollBack();
+            return ApiResponseHelper::rollback($ex);
         }
-        $user->save();
+    }
 
-        return response()->json($user, 200);
+
+    public function update(UpdateUserRequest $request, $id)
+    {
+        $data = [
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password'=> Hash::make($request->password),
+        ];
+        DB::beginTransaction();
+        try {
+            $this->userRepositoryInterface->update($data, $id);
+            DB::commit();
+            return ApiResponseHelper::sendResponse(null,'Record updated succesfully',200);
+        } catch (\Exception $ex) {
+            DB::rollBack();
+            return ApiResponseHelper::rollback($ex);
+        }
     }
 
     public function destroy($id)
     {
-        User::destroy($id);
-        return response()->json(null, 204);
+        $this->userRepositoryInterface->delete($id);
+        return ApiResponseHelper::sendResponse(null,'Record deleted succesfully',200);
     }
 }
